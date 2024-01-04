7fbc6c3dba819f4cb5b4c2afe81f22efd4947919
Charlie_Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-01-04 10:31:12 -0800

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/cjsmocjsmo/sendmail
+
+go 1.19
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/sendgrid/rest v2.6.9+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.14.0+incompatible // indirect
+)
---
+++ sendmail.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "log"
+	"os"
+    "github.com/joho/godotenv"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+    
+)
+
+func main() {
+    // read the sendmail.env file
+    err := godotenv.Load("sendmail.env")
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+    // Define command-line flags
+    etype := flag.String("etype", "com", "Mandatory, choices are com or esti.")
+    msgid := flag.String("mgsid", "", "This is comid or estid")
+    name := flag.String("name", "", "The name to greet")
+    address := flag.String("address", "", "The customers physical address")
+    city := flag.String("city", "", "The customers city")
+    phone := flag.String("phone", "", "The customers phone number")
+    email := flag.String("email", "", "The customers email address")
+    comment := flag.String("comment", "", "The customers comment")
+    intake := flag.String("intake", "", "The intake date")
+    reqdate := flag.String("reqdate", "", "The requested date")
+
+    flag.Parse()
+
+    if *etype == "com" {
+        subJECT := "atsbot: " + *name + " has left a new comment"
+
+        h0 := "<div>"
+        h1 := "<p>Name: " + *name + "</p>"
+        h5 := "<p>Email: " + *email + "</p>"
+        h6 := "<p>Comment: " + *comment + "</p>"
+        h9 := "</div>"
+        h10 := "<a href='http://192.168.0.91:8181/accept/" + *msgid + "'><button>Accept</button></a>"
+        h11 := "<a href='http://192.168.0.94:8181/reject/" + *msgid +"'><button>Reject</button></a>"
+        html_str := h0 + h1 + h5 + h6 + h9 + h10 + h11
+
+        from := mail.NewEmail("atsbot", "porthose.cjsmo.cjsmo@gmail.com")
+        subject := subJECT
+        to := mail.NewEmail("Charlie", "porthose.cjsmo.cjsmo@gmail.com")
+        plainTextContent := ""
+        htmlContent := html_str
+        message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+        client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+        response, err := client.Send(message)
+        if err != nil {
+            log.Println(err)
+        } else {
+            fmt.Println(response.StatusCode)
+            fmt.Println(response.Body)
+            fmt.Println(response.Headers)
+        }
+    } else {
+        subJECT := *name + " has requested an estimate"
+        h0 := "<div>"
+        h1 := "<p>Name: " + *name + "</p>"
+        h2 := "<p>Address: " + *address + "</p>"
+        h3 := "<p>City: " + *city + "</p>"
+        h4 := "<p>Phone: " + *phone + "</p>"
+        h5 := "<p>Email: " + *email + "</p>"
+        h6 := "<p>Comment: " + *comment + "</p>"
+        h7 := "<p>Intake: " + *intake + "</p>"
+        h8 := "<p>ReqDate: " + *reqdate + "</p>"
+        h9 := "</div>"
+        h10 := "<a href='http://192.168.0.91:8181/accept/" + *msgid + "'><button>Completed</button></a>"
+        html_str := h0 + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9 + h10
+        from := mail.NewEmail("atsbot", "porthose.cjsmo.cjsmo@gmail.com")
+        subject := subJECT
+        to := mail.NewEmail("Charlie", "porthose.cjsmo.cjsmo@gmail.com")
+        plainTextContent := ""
+        htmlContent := html_str
+        message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+        client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+        response, err := client.Send(message)
+        if err != nil {
+            log.Println(err)
+        } else {
+            fmt.Println(response.StatusCode)
+            fmt.Println(response.Body)
+            fmt.Println(response.Headers)
+        }
+    }
+
+    
+}
